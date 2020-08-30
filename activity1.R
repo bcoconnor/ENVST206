ffbbafb24d8af8f84b85d3d8833fbd969c7d8da3
brynn <bcoconnor00@gmail.com>
2020-08-30 14:23:17 -0400

    question 3 practice

--- activity1.R
+++ activity1.R
@@ -1,4 +1,23 @@
 #activity 1
 print("hello R world")
 
+#practice
 2*2
+6^6
+5+210
+3-10
+
+#name number variable
+a <- 2446
+#multiply number by 5
+5*a
+a/2
+
+b <- c(2446, 40, 10000)
+b/2
+
+#question 3 practice with equations and vectors
+100/4
+5^2
+b <- c(3456, 80, 22000, 300)
+b*2
