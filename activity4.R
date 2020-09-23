17a4c05ab24b072929654df819c8d61bbf0be356
brynn <bcoconnor00@gmail.com>
2020-09-23 11:34:41 -0400

    question 3 graphs

--- activity4.R
+++ activity4.R
@@ -32,5 +32,65 @@ abline(h=0)
 #print out results
 summary(dam.mod)
 
+#question 2
+#make plot of beaver dams and surface water
+plot(datB$dams.n, datB$area.h, 
+     pch = 19, 
+     col = "royalblue4",
+     ylab = "Surface water area (ha)",
+     xlab =  "Number of beaver dams")
+#add regression line
+#make line width thicker
+abline(dam.mod, lwd=2)
+
+#read in red maple pheno dataset
+pheno <- read.csv("/Users/brynnoconnor/Documents/ENVSTDATA/ao4/red_maple_pheno.csv")
+View(pheno)
+
+#set up panel of plots with one row and two columns
+par(mfrow=c(1,2))
+#doy relation to max temp plot
+plot(pheno$Tmax,pheno$doy, 
+     pch = 19, 
+     col = "royalblue4",
+     ylab = "Day of leaf out",
+     xlab =  "Maximum temperature (C)")
+#doy relation to precipitation plot
+plot(pheno$Prcp,pheno$doy, 
+     pch = 19, 
+     col = "royalblue4",
+     ylab = "Day of leaf out",
+     xlab =  "Precipitation (mm)")
+
+#question 3
+#set up panel of plots with two rows and two columns
+par(mfrow=c(2,2))
+#doy relation to latitude plot
+plot(pheno$Lat,pheno$doy, 
+     pch = 19, 
+     col = "royalblue4",
+     ylab = "Day of leaf out",
+     xlab =  "Latitude")
+#doy relation to elevation plot
+plot(pheno$elev,pheno$doy, 
+     pch = 19, 
+     col = "royalblue4",
+     ylab = "Day of leaf out",
+     xlab =  "Elevation ")
+#doy relation to max temperature plot
+plot(pheno$Tmax,pheno$doy, 
+     pch = 19, 
+     col = "royalblue4",
+     ylab = "Day of leaf out",
+     xlab =  "Maximum temperature (C)")
+#doy relation to site description plot
+plot(pheno$siteDesc,pheno$doy, 
+     pch = 19, 
+     col = "royalblue4",
+     ylab = "Day of leaf out",
+     xlab =  "Maximum temperature (C)")
+
+
+dev.off()
 
 
