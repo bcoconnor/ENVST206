8a4aea2de9c3fe0911aaa36e15448a48b6b76556
brynn <bcoconnor00@gmail.com>
2020-09-24 22:57:44 -0400

    question 6/7

--- activity4.R
+++ activity4.R
@@ -62,30 +62,61 @@ plot(pheno$Prcp,pheno$doy,
      ylab = "Day of leaf out",
      xlab =  "Precipitation (mm)")
 
+
 #question 3
-#doy relation to latitude plot
+#set up panel of plots with 2 rows and 2 columns
+par(mfrow=c(1,2))
+#doy to latitude plot
 plot(pheno$Lat,pheno$doy, 
      pch = 19, 
      col = "royalblue4",
      ylab = "Day of leaf out",
      xlab =  "Latitude")
-#doy relation to elevation plot
+#doy to elevation plot
 plot(pheno$elev,pheno$doy, 
      pch = 19, 
      col = "royalblue4",
      ylab = "Day of leaf out",
-     xlab =  "Elevation ")
-#doy relation to max temperature plot
+     xlab =  "Elevation (m)")
+#doy to max temperature plot
 plot(pheno$Tmax,pheno$doy, 
      pch = 19, 
      col = "royalblue4",
      ylab = "Day of leaf out",
      xlab =  "Maximum temperature (C)")
-#doy relation to site description box plot
+#doy to site description box plot
 boxplot(pheno$doy ~ pheno$siteDesc, xlab ="Site Description (Rural/Urban)", 
      ylab="Day of leaf out")
 
-
+#clear plot panel
 dev.off()
+
+#question 4
+#check for multi-collinearity
+plot( ~  pheno$Lat + pheno$Tmax+ pheno$Tmin +pheno$Prcp + pheno$elev + pheno$siteDesc)
+
+#question 5
+#build and run multiple regression
+pheno$urID <- ifelse(pheno$siteDesc == "Urban",1,0)
+
+#question 6
+#set up multiple regression
+mlr <- lm(pheno$doy ~  pheno$Tmax  + pheno$Prcp + pheno$elev + pheno$urID)
+
+#calculate fitted values from regression line for each observation
+mlFitted <- fitted(mlr)
+
+#set up qq plot to check assumptions
+qqnorm(mlFitted)
+#add qq line to check assumptions
+qqline(mlFitted)
+
+#make residual plot
+plot(mlr, mlFitted, 
+     xlab = "day of leaf out", 
+     ylab = "standardized residual")
+#add a horizontal line at zero
+abline(h=0)
+
 
 
