2d0b0f1e7b1022cda49f82bd1a52fd31d766e10a
brynn <bcoconnor00@gmail.com>
2020-09-28 21:47:57 -0400

    question 3

--- activity5.R
+++ activity5.R
@@ -1,10 +1,12 @@
+library(ggplot2)
+
 #read in weather station file from the data folder
 datW <- read.csv("/Users/brynnoconnor/Documents/ENVSTDATA/ao2/noaa2011124.csv")
 #specify that the name column should be a factor
 datW$NAME<- as.factor(datW$NAME)
 #set up a vector of all names for each level
 nameS <- levels(datW$NAME)
-nameS[2]
+nameS
 
 #make a dataframe with just precipitation, year, and site name
 #remove NA using na.omit
@@ -19,3 +21,155 @@ precip <- aggregate(datP$PRCP, by=list(datP$NAME,datP$year), FUN="sum", na.rm=TR
 colnames(precip) <- c("NAME","year","totalP")
 #add the x column from aggregate looking at the length of observations in each year
 precip$ncount <- aggregate(datP$PRCP, by=list(datP$NAME,datP$year), FUN="length")$x
+
+
+#make a new dataframe, remove years with too many missing precip data
+pr <- precip[precip$ncount >=364, ]
+
+#install.packages("ggplot2") INSATLLED ALREADY
+
+#base r plot
+plot(pr$year, pr$totalP)
+
+ggplot(data = pr, aes(x = year, y=totalP, color=NAME ) )+ #data for plot
+  geom_point()+ #make points at data point
+  geom_path()+ #use lines to connect data points
+  labs(x="year", y="Annual Precipitation") #make axis labels
+
+#look at only livermore california and morrisville new york preciptiation
+ca <- pr[pr$NAME == nameS[2], ]
+ny <- pr[pr$NAME == nameS[5], ]
+
+#make a plot of california precip
+plot(ca$year, ca$totalP)
+
+#make a plot of california precip
+plot(ca$year, ca$totalP,
+     type = "b",
+     pch = 19,
+     ylab = "Annual precipitation (mm)",
+     xlab = "Year")
+
+#make a plot of california precip
+plot(ca$year, ca$totalP,
+     type = "b",
+     pch = 19,
+     ylab = "Annual precipitation (mm)",
+     xlab = "Year", 
+     yaxt = "n")
+
+#add y axis
+#arguments are axis number (1 bottom, 2 left, 3 top, 4 right)
+#las = 2 changes the labels to be read in horizontal direction
+axis(2, seq(200,800, by=200), las=2 )
+
+#adding New York for reference
+plot(ca$year, ca$totalP,
+     type = "b",
+     pch = 19,
+     ylab = "Annual precipitation (mm)",
+     xlab = "Year", 
+     yaxt = "n")
+#add y axis
+axis(2, seq(200,800, by=200), las=2 )
+#add arizona
+points(ny$year, ny$totalP,
+       type = "b",
+       pch = 19,
+       col="tomato3")
+
+#changing axes range to include all data points
+plot(ca$year, ca$totalP,
+     type = "b",
+     pch = 19,
+     ylab = "Annual precipitation (mm)",
+     xlab = "Year", 
+     yaxt = "n",
+     ylim =c(0, 1600))
+#add y axis
+axis(2, seq(0,1600, by=400), las=2 )
+#add new york
+points(ny$year, ny$totalP,
+       type = "b",
+       pch = 19,
+       col="tomato3")
+
+#full plot ca and ny
+plot(ca$year, ca$totalP,
+     type = "b",
+     pch = 19,
+     ylab = "Annual precipitation (mm)",
+     xlab = "Year", 
+     yaxt = "n",
+     ylim =c(0, 1600))
+#add y axis
+axis(2, seq(0,1600, by=400), las=2 )
+#add arizona
+points(ny$year, ny$totalP,
+       type = "b",
+       pch = 19,
+       col="tomato3")
+
+#add legend
+legend("topleft", #position
+       c("California", "New York"), #labels
+       col= c("black", "tomato3"), #colors
+       pch=19, #point shape
+       lwd=1, #line thickness 1, anytime both point & line arguments are given both will be drawn
+       bty="n") #always use this argument otherwise an ugly box is drawn
+
+
+
+#question 3 - plot ny and nd
+#make a dataframe with just temperature, year, and site name
+#remove NA using na.omit
+datT <- na.omit(data.frame(NAME=datW$NAME,
+                           year=datW$year,
+                           TMAX=datW$TMAX))
+
+#total mean temperature
+TMAX <- aggregate(datW$TMAX, by=list(datW$NAME,datW$year), FUN="mean", na.rm=TRUE)
+#use aggregate to get mean temperatures
+TMAX <- aggregate(datT$TMAX, by=list(datT$NAME,datT$year), FUN="mean", na.rm=TRUE)
+#rename columns
+colnames(TMAX) <- c("NAME","year","TMAX")
+#add the x column from aggregate looking at the length of observations in each year
+TMAX$ncount <- aggregate(datT$TMAX, by=list(datT$NAME,datT$year), FUN="length")$x
+
+#make a new dataframe, remove years with too many missing temp data
+temp <- TMAX[TMAX$ncount >=364, ]
+
+ggplot(data = TMAX, aes(x = year, y=TMAX, color=NAME ) )+ #data for plot
+  geom_point()+ #make points at data point
+  geom_path()+ #use lines to connect data points
+  labs(x="year", y="Max Temp") #make axis labels
+
+nd <- temp[temp$NAME == nameS[3], ]
+ny <- temp[temp$NAME == nameS[2], ]
+
+#add north dakota
+plot(nd$year, nd$TMAX,
+     type = "b",
+     pch = 19,
+     ylab = "Annual Temperature (mm)",
+     xlab = "Year", 
+     yaxt = "n",
+     ylim =c(5, 30))
+#add y axis
+axis(2, seq(5,30, by=5), las=2 )
+#add new york
+points(ny$year, ny$TMAX,
+       type = "b",
+       pch = 19,
+       col="tomato3")
+
+#add legend
+legend("topleft", #position
+       c("North Dakota", "New York"), #labels
+       col= c("black", "tomato3"), #colors
+       pch=19, #point shape
+       lwd=1, #line thickness 1, anytime both point & line arguments are given both will be drawn
+       bty="n") #always use this argument otherwise an ugly box is drawn
+
+
+
