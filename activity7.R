1eb78e05ccd0057093374fd5c54c2b4b49c2885e
brynn <bcoconnor00@gmail.com>
2020-10-21 23:21:58 -0400

    more datasets and plots

--- activity7.R
+++ activity7.R
@@ -11,11 +11,16 @@ volcanic <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Bedrock/Volcani
 faults <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Bedrock/Fault.shp")
 glacier <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Surficial/Glacial_area_feat_boundaries.shp")
 volcanic2 <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Surficial/Volcanic_point_feats.shp")
-plot(caldera, col = "lightblue2", border="grey50")
-plot(geothermal, col = "lightblue2", border="grey50")
 
 head(geothermal@data)
 head(caldera@data)
+head(volcanic@data)
+head(faults@data)
+head(glacier@data)
+head(volcanic2@data)
+
+plot(caldera, col = "lightblue2", border="grey50")
+plot(geothermal, col = "lightblue2", border="grey50")
 
 plot(glacier, col="black")
 plot(volcanic22, col="pink")
