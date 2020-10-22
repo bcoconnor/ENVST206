303c35d18536c2fc6cc491d27db96fa6bab6fc5c
brynn <bcoconnor00@gmail.com>
2020-10-21 22:51:33 -0400

    datasets and plots

--- activity7.R
+++ activity7.R
@@ -1,5 +1,3 @@
-#install.packages(c("sp","rgdal","dplyr"))
-#install.packages(c("rgdal"))
 #package for vector data
 library(sp)
 #package for reading in spatial data
@@ -7,12 +5,28 @@ library(rgdal)
 #data manangement package
 library(dplyr)
 
-#read in shapefiles
-#readOGR in rgdal does this
-#glaciers in 1966
-geothermal <- readOGR("/Users/brynnoconnor/Downloads/Geothermal/Geothermal.shp")
-
-
+geothermal <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Geothermal/Geothermal.shp")
+caldera <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Caldera/calderas.shp")
+volcanic <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Bedrock/Volcanic_point_features.shp")
+faults <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Bedrock/Fault.shp")
+glacier <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Surficial/Glacial_area_feat_boundaries.shp")
+volcanic2 <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Surficial/Volcanic_point_feats.shp")
+plot(caldera, col = "lightblue2", border="grey50")
 plot(geothermal, col = "lightblue2", border="grey50")
 
 head(geothermal@data)
+head(caldera@data)
+
+plot(glacier, col="black")
+plot(volcanic22, col="pink")
+plot(geothermal, col="green", add=TRUE)
+plot(volcanic, col="purple", add=TRUE)
+plot(caldera, col="tomato3", add=TRUE)
+plot(faults, col="skyblue", add=TRUE)
+
+#faults and caldera have relationship
+#volcanic and faults?
+#geothermal and volcanic
+#volcanic points vs volcanic 
+
+
