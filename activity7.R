4883b65ec95a696158542e9b0a7d8601bf6c11f7
brynn <bcoconnor00@gmail.com>
2020-10-23 10:57:34 -0400

    activity 8

--- activity7.R
+++ activity7.R
@@ -11,6 +11,8 @@ volcanic <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Bedrock/Volcani
 faults <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Bedrock/Fault.shp")
 glacier <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Surficial/Glacial_area_feat_boundaries.shp")
 volcanic2 <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Surficial/Volcanic_point_feats.shp")
+streams <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Hydrography/Streams.shp")
+lakes <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Hydrography/Lakes.shp")
 
 head(geothermal@data)
 head(caldera@data)
@@ -18,16 +20,29 @@ head(volcanic@data)
 head(faults@data)
 head(glacier@data)
 head(volcanic2@data)
+head(streams@data)
+head(lakes@data)
+
+#look at just areas of different geothermal areas
+geothermal_area <- data.frame( thermal_area = geothermal$THERM_AREA, 
+                               geo_area = geothermal@data$LOCATION,
+                                area = geothermal@data$AREA)
+
+#average of all geothermal areas
+mean(geothermal_area$area)
 
 plot(caldera, col = "lightblue2", border="grey50")
 plot(geothermal, col = "lightblue2", border="grey50")
 
-plot(glacier, col="black")
-plot(volcanic22, col="pink")
+plot(glacier, col="green", add=TRUE)
+plot(streams, col="black")
+plot(lakes, col="black")
+plot(volcanic2, col="pink")
 plot(geothermal, col="green", add=TRUE)
-plot(volcanic, col="purple", add=TRUE)
+plot(volcanic, col="purple")
 plot(caldera, col="tomato3", add=TRUE)
 plot(faults, col="skyblue", add=TRUE)
+
 
 #faults and caldera have relationship
 #volcanic and faults?
---
+++ activity8.R
@@ -0,0 +1,38 @@
+#install.packages(c("raster"))
+
+library(raster)
+library(ggplot2)
+library(rgdal)
+
+#set up directory for oneida data folder
+dirR <- "/Users/brynnoconnor/Documents/ENVSTDATA/ao8/a08/oneida"
+
+#read in Sentinel data
+
+rdatB2 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B02_20m.tif"))
+rdatB3 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B03_20m.tif"))
+rdatB4 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B04_20m.tif"))
+rdatB8 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B08_20m.tif"))
+
+plot(rdatB2/10000)
+
+#stack red green blue, divide to convert to reflectance
+rgbS <- stack(rdatB4,rdatB3,rdatB2)/10000
+
+#view raster, a few pixels in blue have reflectanc above 1 so set scale
+plotRGB(rgbS, scale=2)
+
+#don't need the scale argument when adding in the contrast stretch
+plotRGB(rgbS, stretch="lin")
+
+#full resolutions 
+#get the total number of pixels by multiplying the number of rows and columns
+#in the raster
+plotRGB(rgbS, stretch="lin",maxpixels=rgbS@nrows*rgbS@ncols)
+
+
+
+
+
+
+
