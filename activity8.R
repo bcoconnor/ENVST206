9390ecb87115a058ca8b6dbec99979f6cf953be6
brynn <bcoconnor00@gmail.com>
2020-10-27 16:25:22 -0400

    question 5-6

--- activity8.R
+++ activity8.R
@@ -31,6 +31,69 @@ plotRGB(rgbS, stretch="lin")
 plotRGB(rgbS, stretch="lin",maxpixels=rgbS@nrows*rgbS@ncols)
 
 
+#question 4
+rgbS2 <- stack(rdatB3,rdatB2,rdatB4)/10000
+#view raster, a few pixels in blue have reflectance above 1 so set scale
+plotRGB(rgbS2, scale=2)
+#don't need the scale argument when adding in the contrast stretch
+plotRGB(rgbS2, stretch="lin")
+
+
+#question 5
+#calculate NDVI
+#NIR-red/(NIR + RED)
+NDVI <- (rdatB8 - rdatB4) / (rdatB8 + rdatB4)
+#visualize NDVI across the Oneida lake area
+plot(NDVI)
+
+
+#read in landcover points data
+#you may have to change your slash direction if you are on a windows computer
+#I've also turned off the info print out here when you read in the file
+algae <- readOGR(paste0(dirR,"/Oneida/algae.shp"), verbose=FALSE)
+agri <- readOGR(paste0(dirR,"/Oneida/agriculture.shp"), verbose=FALSE)
+forest <- readOGR(paste0(dirR,"/Oneida/forest.shp"), verbose=FALSE)
+water <- readOGR(paste0(dirR,"/Oneida/water.shp"), verbose=FALSE)
+wetlands <- readOGR(paste0(dirR,"/Oneida/wetlands.shp"), verbose=FALSE)
+
+
+#question 6
+#plot points and true color
+plotRGB(rgbS, stretch="lin",maxpixels=2297430)
+plot(algae, add=TRUE, col=rgb(0.5,0.5,0.5,0.5), pch=19)
+plot(agri, add=TRUE, col=rgb(0.75,0.5,0.5,0.5), pch=19)
+plot(forest, add=TRUE, col=rgb(0.75,0.75,0.25,0.5), pch=19)
+plot(water, add=TRUE, col=rgb(0.33,0.75,0.75,0.5), pch=19)
+plot(wetlands, add=TRUE, col=rgb(0.33,0.33,0.65,0.5), pch=19)
+legend("bottomleft", c("algae","agri","forest","water","wetlands"),
+       pch=19, col=c(rgb(0.5,0.5,0.5,0.5),rgb(0.75,0.5,0.5,0.5),rgb(0.75,0.75,0.25,0.5),rgb(0.33,0.75,0.75,0.5),rgb(0.33,0.33,0.65,0.5)),
+       bty="n", cex=0.75)
+
+#set up a dataframe with all of the point coordinates
+landExtract <-  data.frame(landcID = as.factor(rep(c("algae","water","agri","forest","wetland"),each=120)),
+                           x=c(algae@coords[,1],water@coords[,1],agri@coords[,1],forest@coords[,1],wetlands@coords[,1]),
+                           y=c(algae@coords[,2],water@coords[,2],agri@coords[,2],forest@coords[,2],wetlands@coords[,2]))
+help(rep)
+
+
+#stack all bands
+allbands <-  stack(rdatB2, rdatB3, rdatB4,rdatB8)/10000
+#add the raster reflectance values to the point coordinates and classes
+#extract(raster, matrix of coordinates)
+#raster:: helps ensure that extract comes from the raster package
+ExtractOut <- raster::extract(allbands,landExtract[,2:3])
+#name the bands
+colnames(ExtractOut) <- c("B02","B03","B04","B08")
+#combine the original data with the coordinates with the raster data
+rasterEx <- cbind(landExtract,ExtractOut)
+#look at data
+head(rasterEx)
+
+
+ggplot(data=rasterEx, aes(x=B02, y=B03, color=landcID))+
+  geom_point(alpha=0.6)+
+  theme_classic()
+
 
 
 
