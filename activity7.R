a148daf0048d791d1bdfa14e712610612e016d4c
brynn <bcoconnor00@gmail.com>
2020-11-16 21:52:08 -0500

    crop chart and chisq test

--- activity7.R
+++ activity7.R
@@ -13,23 +13,19 @@ caldera <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Caldera/calderas
 volcanic <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Bedrock/Volcanic_point_features.shp")
 faults <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Bedrock/Fault.shp")
 glacier <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Surficial/Glacial_area_feat_boundaries.shp")
-volcanic2 <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Surficial/Volcanic_point_feats.shp")
 streams <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Hydrography/Streams.shp")
-lakes <- readOGR("/Users/brynnoconnor/Documents/ENVSTDATA/ao7/Hydrography/Lakes.shp")
 
 head(geothermal@data)
 head(caldera@data)
 head(volcanic@data)
 head(faults@data)
 head(glacier@data)
-head(volcanic2@data)
 head(streams@data)
-head(lakes@data)
 
 #look at just areas of different geothermal areas
-geothermal_area <- data.frame( thermal_area = geothermal$THERM_AREA, 
+geothermal_area <- data.frame( thermal_area = geothermal$THERM_AREA,
                                geo_area = geothermal@data$LOCATION,
-                                area = geothermal@data$AREA)
+                               area = geothermal@data$AREA)
 
 #average of all geothermal areas
 mean(geothermal_area$area)
@@ -39,28 +35,47 @@ plot(geothermal, col = "lightblue2", border="grey50")
 
 plot(glacier, col="green", add=TRUE)
 plot(streams, col="black")
-plot(lakes, col="black")
-plot(volcanic2, col="pink")
 plot(geothermal, col="green", add=TRUE)
 plot(volcanic, col="purple")
 plot(caldera, col="tomato3", add=TRUE)
 plot(faults, col="skyblue", add=TRUE)
 
 
-#faults and caldera have relationship
-#volcanic and faults?
-#geothermal and volcanic
-#volcanic points vs volcanic 
-
-help(crop)
-
 plot(volcanic2,
      main = "Shapefile imported into R - crop extent",
      axes = TRUE,
      border = "blue")
 
-# crop the lidar raster using the vector extent
-geothermal_crop <- crop(geothermal, volcanic2)
+#create new dataframe
+geo_results <- data.frame("Feature"=NA, "Inside"=NA, "Outside"=NA)
+#remove empty row at top
+geo_results <- na.omit(geo_results)
+
+# crop volcanic2 raster using the vector extent
+caldera_crop <- crop(geothermal, caldera)
+caldera_result <- c("Caldera", NROW(caldera_crop), NROW(caldera))
+geo_results <- rbind(geo_results, caldera_result)
+
+volcanic_crop <- crop(geothermal, volcanic)
+volcanic_result <- c("Volcanic Points", NROW(volcanic_crop), NROW(volcanic))
+geo_results <- rbind(geo_results, volcanic_result)
+
+faults_crop <- crop(geothermal, faults)
+faults_result <- c("Faults", NROW(faults_crop), NROW(faults))
+geo_results <- rbind(geo_results, faults_result)
+
+glacier_crop <- crop(geothermal, glacier)
+glacier_result <- c("Glacier", NROW(glacier_crop), NROW(glacier))
+geo_results <- rbind(geo_results, glacier_result)
+
+streams_crop <- crop(geothermal, streams)
+streams_result <- c("Streams", NROW(streams_crop), NROW(streams))
+geo_results <- rbind(geo_results, streams_result)
+
+
+
+
+
 plot(geothermal_crop, main = "Cropped geothermal areas")
 
 # add shapefile on top of the existing raster
@@ -68,14 +83,12 @@ plot(volcanic2, add = TRUE)
 
 
 #how to convret spatial data to a dataframe
-#(SpatialDataPointsFrame)
-chisq.test(geothermal_area)
+chisq.test(geo_results$inside, geo_results$outside)
 help("chisq.test")
 
 
-
 plot(faults,
-     main = "Shapefile imported into R - crop extent",
+     main = "Shapefile imported into R - faults",
      axes = TRUE,
      border = "blue")
 
@@ -85,10 +98,3 @@ plot(geothermal_crop_faults, main = "Cropped geothermal areas")
 
 # add shapefile on top of the existing raster
 plot(faults, add = TRUE)
-
-
-
-
-
-
-
